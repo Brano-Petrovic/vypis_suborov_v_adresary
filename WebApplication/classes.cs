f31a72bdd80d207cc3c4034da43d0f86940cebea
Brano-Petrovic <68059369+Brano-Petrovic@users.noreply.github.com>
2020-08-12 22:07:54 +0200

    Complete

---
+++ WebApplication/classes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication
+{
+    public static class old_files
+    {
+        static string file_path;
+        static List<string> name_files = new List<string>();
+        static List<int> version_files;
+        static List<DateTime> date_modified_files;
+
+        public static string path
+        {
+            get
+            {
+                return file_path;
+            }
+            set
+            {
+                file_path = value;
+            }
+        }
+        public static List<string> names
+        {
+            get
+            {
+                return name_files;
+            }
+            set
+            {
+                name_files = value;
+            }
+        }
+
+        public static List<DateTime> dates
+        {
+            get
+            {
+                return date_modified_files;
+            }
+            set
+            {
+                date_modified_files = value;
+            }
+        }
+
+        public static List<int> versions
+        {
+            get
+            {
+                return version_files;
+            }
+            set
+            {
+                version_files = value;
+            }
+        }
+    }
+
+}
---
+++ WebApplication/default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Výpis změn v adresáři</h1>
+
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="path_label" runat="server" Text="Zadaj cestu k adresáři (Napr.: D:/test/):"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="input_file_path" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="input_file_path" ErrorMessage="Nezadali ste cestu!"></asp:RequiredFieldValidator>
+        </div>
+        <div >
+            <br />
+            <asp:Button ID="Button_zobrazit" runat="server" Text="Zobrazit subory" OnClick="zobrazit" />
+        </div>
+            <p>
+            <asp:Label ID="vypis_aktualny" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
